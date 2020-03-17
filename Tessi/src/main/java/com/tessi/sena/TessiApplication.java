ea33ac65dfaf6c007958203ba386ef1b1aa80d03
fabian <fabian@fabian-ThinkPad-Edge-E431>
2020-03-16 21:06:05 -0500

    Commit incial. Close #1

---
+++ Tessi/src/main/java/com/tessi/sena/TessiApplication.java
@@ -0,0 +1,13 @@
+package com.tessi.sena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TessiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TessiApplication.class, args);
+	}
+
+}
