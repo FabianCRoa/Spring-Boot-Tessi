f11a432f6fbc3dbbf838288d070f92ac57c2b406
fabian <fabian@fabian-ThinkPad-Edge-E431>
2020-03-16 21:06:05 -0500

    Commit incial

---
+++ Tessi/src/test/java/com/tessi/sena/TessiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tessi.sena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TessiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
